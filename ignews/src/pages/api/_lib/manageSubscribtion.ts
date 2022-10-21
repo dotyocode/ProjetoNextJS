aa6ae5322832e5c8932a0d483875eeddb154a8b9
Jhonathan Peres <jhonathan.peres@squadra.com.br>
2022-10-21 17:24:14 -0300

    configure manage subscription stripe

---
+++ ignews/src/pages/api/_lib/manageSubscribtion.ts
@@ -0,0 +1,38 @@
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../service/fauna';
+import { stripe } from '../../../service/stripe';
+
+export async function saveSubscription(subscriptionId: string, customerId: string) {
+  //salvando informações de pagamento no banco de dados
+  //crie primeiro um indice no fauna
+
+  //buscando usuario no banco do fauna com o ID CustomerID [stripeCustumerID]
+    const userRef = await fauna.query(
+     q.Select(
+        "ref",
+        q.Get(
+          q.Match(
+            q.Index(
+            'user_by_stripe_customer_id'), 
+            customerId
+        ))
+      )
+    )
+  //obtendo todas informações do Subscription
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+  //salvar os dados da subscription do usuario no faunaDB
+    //dados que vão ser salvos no banco de dados do fauna
+    const subscriptionData = {
+      id: subscription.id,
+      userId: userRef,
+      status: subscription.status,
+      price_id: subscription.items.data[0].price.id,      
+    }
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
+    )
+}
---
+++ ignews/src/pages/api/webhooks.ts
@@ -0,0 +1,76 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+
+import { stripe } from '../../service/stripe';
+import { saveSubscription } from './_lib/manageSubscribtion';
+
+//configurando streeming do stripe - transforma em string
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+    return Buffer.concat(chunks)
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+//configurando os evento relevantes
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
+
+export default  async (req: NextApiRequest, res: NextApiResponse) => {
+  //configurando streeming do stripe
+  // todos dados da requisicao está aqui em buff
+  if(req.method === 'POST') {
+    const buff = await buffer(req)
+    const secret = req.headers['stripe-signature']
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buff, secret, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch (e) {
+      return res.status(400).send(`WEBHOOK ERROR: ${e.message}`)
+    }
+
+    //retorna o que é visto no terminal do stripe
+    const { type } = event;
+
+    if(relevantEvents.has(type)) {
+
+      try {
+        //ouvindo os eventos
+        switch(type) {
+          case 'checkout.session.completed':
+            //salvando o usuario
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+            await saveSubscription(checkoutSession.subscription.toString(), checkoutSession.customer.toString())
+
+            break;
+        // não houve tratativa
+          default:
+          throw new Error('Unhandled Event')
+        }
+      } catch (e) {
+          //retornando isso para o stripe
+         return res.json({error: 'Webhook handle failed'})
+        }
+    }
+
+    console.log('evento recebido')
+    res.json({recebido: true})
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
+
+}
